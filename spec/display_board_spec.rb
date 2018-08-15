6704a5afae2777c7f0565896e940f40b091cf50c
Allison Howard <alliemhow@gmail.com>
2018-08-15 23:30:47 +0000

    automatically backed up by learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -172,7 +172,7 @@ describe "#display_board in 'lib/display_board.rb" do
       expect(rows[4]).to eq(" X | X | X ")
 
       # *** Comment the line below by adding a # at the line start ***
-      expect(true).to be(true)
+      # expect(true).to be(true)
     end
 
     it 'prints an entire board full of Os' do
@@ -182,6 +182,8 @@ describe "#display_board in 'lib/display_board.rb" do
       # Hint: You should be able to copy the code in the previous it example
       # and make a few simple edits to convert the previous example to this
       # example's situation.
+      
+      
 
       # *** Comment the line below by adding a # at the line start ***
       expect(true).to be(true)
